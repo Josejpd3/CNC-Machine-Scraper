6c70640d8747e51f1c74e58640e8bd735881ec1f
Jose Plasencia <josejpd3@gmail.com>
2024-10-26 18:48:13 -0400

    Update layout.js to use client

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,13 @@
+'use client'
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { Navigation } from '@/components/Navigation'
-
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'CNC Scraper',
-  description: 'A web scraper for CNC machines',
-}
+
 
 export default function RootLayout({ children }) {
+
   return (
     <html lang="en">
       <body className={inter.className}>
